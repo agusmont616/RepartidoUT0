6426541c33f2a6d9c5471c2870b500a058bb9230
Agustín Montaña Larralde <agustinmont2001@gmail.com>
2026-03-13 22:20:29 -0300

    "Correciones"

--- ejercicio1/src/main/java/com/repartidout0/App.java
+++ ejercicio1/src/main/java/com/repartidout0/App.java
@@ -7,26 +7,38 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Variable con nombre válido
+        // *** Variable con nombre válido ***
         PruebaAtributos prueba = new PruebaAtributos();
         // Imprimir los atributos con sus valores por defecto (variables no inicializadas)
         prueba.imprimirAtributos();
 
-        // Variable con nombre inválido (comentada para evitar error de compilación)
+        // *** Variable con nombre inválido (comentada para evitar error de compilación) ***
 
-        PruebaAtributos static = new PruebaAtributos(); //
-        static.imprimirAtributos();
+        // PruebaAtributos static = new PruebaAtributos(); //
+        // static.imprimirAtributos();
 
-        // DEVUELVE:    Syntax error on token "static", delete this token
-        //              Syntax error on token "static", invalid Expression
+        /*
+        DEVUELVE:   Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
+                      PruebaAtributos cannot be resolved to a variable
+                      Syntax error on token "static", delete this token
+                      Syntax error on token "static", invalid Expression 
+                      */
+        
 
-        // Imprimir variables locales sin inicializar
+
+
+        // *** Imprimir variables locales sin inicializar ***
 
         //int numero;
         // System.out.println("Número: " + numero);
 
-        // DEVUELVE:    Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
-        //               The local variable numero may not have been initialized
+        /*
+        DEVUELVE:   Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+                      The local variable numero may not have been initialized
+                        */
+         
+
+
     
     }
 }
